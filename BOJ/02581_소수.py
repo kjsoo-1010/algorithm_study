50fc3deeed3e89f4d2f5a9be791031a1cacd33e2
kjsoo-1010 <81964758+kjsoo-1010@users.noreply.github.com>
2021-07-12 01:29:29 +0900

    Create 02581_소수.py

---
+++ BOJ/02581_소수.py
@@ -0,0 +1,24 @@
+import math
+M = int(input())
+N = int(input())
+odd = []
+odd_check = True
+for i in range(M, N+1) :
+  if i == 1 :
+    odd_check = False
+  elif i == 2 :
+    odd_check = True
+  else :
+    for j in range(2, i) :
+      odd_check = True
+      if i % j == 0 :
+        odd_check = False
+        break
+  if odd_check == True :
+    odd.append(i)
+if len(odd) == 0 :
+  print(-1)
+else :
+  #print(odd)
+  print(sum(odd))
+  print(odd[0])
