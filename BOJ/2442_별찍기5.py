232d9f5b8372988e8ee3cdfd718a40298566aa9a
kjsoo-1010 <81964758+kjsoo-1010@users.noreply.github.com>
2021-07-11 23:36:12 +0900

    Update 2442_별찍기5.py

--- BOJ/2442_별찍기5.py
+++ BOJ/2442_별찍기5.py
@@ -1,9 +1,3 @@
 n = int(input())
-h = n//2
-for i in range(0, n) :
-    for j in range((-2)*h, 2*h+1) :
-        if (-j<=i) and (j<=i):
-            print("*", end = '')
-        elif (j <= i) :
-            print(" ", end = '')
-    print()
+for i in range(1, n+1):
+    print(' ' * (n-i) +'*'*((2*i)-1))
