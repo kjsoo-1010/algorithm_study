d0d0f03501a4be1e3233d325c43198f3db50a76c
kjsoo-1010 <81964758+kjsoo-1010@users.noreply.github.com>
2021-07-17 18:13:58 +0900

    Update 06996_애너그램.cpp

--- BOJ/06996_애너그램.cpp
+++ BOJ/06996_애너그램.cpp
@@ -1,5 +1,27 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i<n; i++) {
+        string s1, s2;
+        cin >> s1 >> s2;
+        string w1 = s1;
+        string w2 = s2;
+        sort(w1.begin(), w1.end());
+        sort(w2.begin(), w2.end());
+        if (w1 == w2) cout << s1 << " & " << s2 << " are anagrams."<<endl;
+        else cout << s1 << " & " << s2 << " are NOT anagrams." << endl;
+    }
+}
+
+/*
+#include <iostream>
+#include <string>
 #include <set>
 using namespace std;
 
@@ -22,3 +44,4 @@ int main() {
 	}
 	return 0;
 }
+*/
