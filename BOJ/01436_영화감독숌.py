df0ae6385f925fa96de6889462ea9270c3a3c5e2
kjsoo-1010 <tn1078@kookmin.ac.kr>
2022-07-01 22:23:32 +0900

    01436_영화감독숌.py

---
+++ BOJ/01436_영화감독숌.py
@@ -0,0 +1,26 @@
+'''
+n = int(input())
+
+if n<=6:
+    print()
+else:
+    n -= 7
+    share = i // 9 
+    if share % 2 == 0:
+        print()
+    else:
+        print()
+'''
+
+n = int(input())
+i = 666
+cnt = 0
+# s = set()
+while(True):
+    if '666' in str(i):
+        cnt += 1
+    if cnt == n:
+        print(i)
+        break
+    i += 1
+
