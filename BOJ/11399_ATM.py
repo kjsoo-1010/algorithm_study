f5ea8ea605c4460ac16e695255b4cb578a5a622b
kjsoo-1010 <tn1078@kookmin.ac.kr>
2022-07-10 04:40:02 +0900

    11399.py

---
+++ BOJ/11399_ATM.py
@@ -0,0 +1,11 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+li = list(map(int, input().split()))
+total = 0
+li.sort(reverse = True)
+for i in range(n):
+    total += li[i] * (i+1)
+print(total)
